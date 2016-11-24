3372ac17bcd251cea1348e463ec8158d8fef6357
Tomáš Hrabal <fil.hrabal@gmail.com>
2016-11-24 13:09:49 +0100

    [FIX] Mention class init

--- lib/elasticsearch_rasi/mention.rb
+++ lib/elasticsearch_rasi/mention.rb
@@ -2,12 +2,14 @@
 require_relative 'document'
 class ElasticsearchRasi
   class Mention < Document
-    def initialize(es, config)
-      super(es, config.merge(
-        idx_read:  config[:idx_mention_read],
-        idx_write: config[:idx_mention_write],
-        alias:     config[:mention_alias],
-        type:      config[:mention_type] || 'document'))
+    def initialize(es, config, es_another)
+      super(es,
+        config.merge(
+          idx_read:  config[:idx_mention_read],
+          idx_write: config[:idx_mention_write],
+          alias:     config[:mention_alias],
+          type:      config[:mention_type] || 'document'),
+        es_another)
     end
   end
 end
