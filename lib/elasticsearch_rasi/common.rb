6da043ca43093191b2172ff09df47e61f75a4391
Tomáš Hrabal <hrabal.tomas@gmail.com>
2020-02-12 14:48:17 +0100

    FIX ignore_max_age

--- lib/elasticsearch_rasi/common.rb
+++ lib/elasticsearch_rasi/common.rb
@@ -33,13 +33,15 @@ module ElasticsearchRasi
 
       def prepare_index(index, doc, max_age, write_date, lang_index, languages)
         return index if doc.blank?
+
         index = prepare_index_language(index, doc, lang_index, languages)
         return nil unless index
+
         prepare_index_date(index, doc, max_age, write_date)
       end
 
       def prepare_read_index(index, read_date, read_date_months, ignore_max_age = false)
-        return index if !read_date || !ignore_max_age
+        return index if !read_date || ignore_max_age
 
         read_date_months.map { |date| "#{index}#{date}" }.uniq.join(',')
       end
